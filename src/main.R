82a2f61c69585ac849967df705f3f131a3448182
Cristian Abrante <cristian@abrante.me>
2019-12-19 18:48:08 +0000

    Merge conflict in main solved

--- src/main.R
+++ src/main.R
@@ -20,35 +20,33 @@ dataset %>% ggplot(aes(x = Age, y = log(AlcoholDrink5Last30d))) + ylim(0, 5) + g
 
 # Preprocesing, choosing the right variables and handling NA Values.
 
-q1_dataset <- DataCombine::DropNA(dataset, Var="AlcoholAmountAvgPerMonth")
+q1_dataset <- DataCombine::DropNA(dataset, Var = "AlcoholAmountAvgPerMonth")
 
 
 
 
 
-<<<<<<< HEAD
-=======
 ## ??? ----
 
 # STEFANO
 # alcohol related variable: SpendTimeBar7d, AlcoholDrink5Last30d, AlcoholAmountAvgPerMonth, AlcoholAmountAvgPerMonth
 # Looking for correlation between alcohol and drugs
-dataset %>% ggplot(aes(x = SmokedCigsLast30d, 
+dataset %>% ggplot(aes(x = SmokedCigsLast30d,
                        y = AlcoholDrink5Last30d)) +
   geom_hex()
 
-dataset %>% ggplot(aes(x = MarijuanaLast30d, 
+dataset %>% ggplot(aes(x = MarijuanaLast30d,
                        y = AlcoholDrink5Last30d)) +
   geom_hex() +
   geom_smooth(method = lm, se = FALSE)
 
-dataset %>% ggplot(aes(x = CocaineLast30d, 
+dataset %>% ggplot(aes(x = CocaineLast30d,
                        y = AlcoholDrink5Last30d)) +
   geom_hex() +
   geom_smooth(method = lm, se = FALSE)
 
 # useless
-dataset %>% ggplot(aes(x = HeroineLast30d, 
+dataset %>% ggplot(aes(x = HeroineLast30d,
                        y = AlcoholDrink5Last30d)) +
   geom_point()
 
@@ -70,19 +68,19 @@ dataset_drug$DrugLast30d <- (dataset_drug$MarijuanaLast30d | dataset_drug$Cocain
 # Same as before but it takes into account also Cigarettes
 dataset_drug$Drug_CigsLast30d <- (dataset_drug$MarijuanaLast30d | dataset_drug$CocaineLast30d | dataset_drug$HeroineLast30d | dataset_drug$SmokedCigsLast30d)
 
-ggplot(data=dataset_drug, aes(x=DrugLast30d, y = AlcoholDrink5Last30d)) +
-  geom_bar(stat="identity", width=0.5)
+ggplot(data = dataset_drug, aes(x = DrugLast30d, y = AlcoholDrink5Last30d)) +
+  geom_bar(stat = "identity", width = 0.5)
 
-ggplot(data=dataset_drug, aes(x=Drug_CigsLast30d, y = AlcoholDrink5Last30d)) +
-  geom_bar(stat="identity", width=0.5)
+ggplot(data = dataset_drug, aes(x = Drug_CigsLast30d, y = AlcoholDrink5Last30d)) +
+  geom_bar(stat = "identity", width = 0.5)
 
 library(GGally)
 # Looking for correlations between drugs
-df <- dataset %>% select('AlcoholDrink5Last30d','SmokedCigsLast30d','MarijuanaLast30d','CocaineLast30d','HeroineLast30d')
+df <- dataset %>% select('AlcoholDrink5Last30d', 'SmokedCigsLast30d', 'MarijuanaLast30d', 'CocaineLast30d', 'HeroineLast30d')
 ggpairs(df)
 
 # Looking for correlation between income and alcohol consumption
-df_2 <- dataset %>% select('AlcoholDrink5Last30d','MonthlyFamilyIncome','FamilyPovertyIndex')
+df_2 <- dataset %>% select('AlcoholDrink5Last30d', 'MonthlyFamilyIncome', 'FamilyPovertyIndex')
 ggpairs(df_2)
 
 # Looking if income affects Alcohol consumption
@@ -92,4 +90,3 @@ ggplot(data = dataset) +
 
 ggplot(data = dataset) +
   geom_hex(mapping = aes(x = MonthlyFamilyIncome, y = AlcoholAmountAvgPerMonth))
->>>>>>> 0ee6dd17a068d2f00fe2c17e8fc3c5cba00c2964
