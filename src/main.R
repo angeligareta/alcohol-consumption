2cca3ab0d95f8f1397bab75881a46b677bad41e6
Cristian Abrante <cristian@abrante.me>
2019-12-19 18:42:02 +0000

    Remove na from alcohol consumption

--- src/main.R
+++ src/main.R
@@ -1,4 +1,6 @@
 library(ggplot2)
+library(tidyr)
+library(DataCombine)
 
 source("./preprocessing/preprocess.R")
 
@@ -12,6 +14,18 @@ dataset <- preprocessed_dataset
 
 ## Alcohol and Sex ----
 ## TODO: Handle NA values
-dataset %>% ggplot(aes(x = Age, y = log(AlcoholDrink5Last30d))) + ylim(0, 5) + geom_boxplot(aes(color = MaritalStatus)) 
+dataset %>% ggplot(aes(x = Age, y = log(AlcoholDrink5Last30d))) + ylim(0, 5) + geom_boxplot(aes(color = MaritalStatus))
 
-## ??? ----
+# Relation beween mental situation, mental illnesses, economic situation and alcohol consumption ----
+
+# Preprocesing, choosing the right variables and handling NA Values.
+
+na.omit(dataset, cols="AlcoholAmountAvgPerMonth")
+
+q1_dataset <- DataCombine::DropNA(dataset, Var="AlcoholAmountAvgPerMonth")
+
+q1_dataset <- dataset  %>% drop_na(AlcoholAmountAvgPerMonth)
+
+
+
+
